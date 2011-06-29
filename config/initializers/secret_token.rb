d613df20c3a965be18fe69a69eb8f77803e95f16
Alex Crichton <alex@alexcrichton.com>
2011-06-29 00:30:07 -0700

    Use SECRET_TOKEN in env if possible

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Crosswords::Application.config.secret_token = '23971fce09f701c42949d73b3fb06bd90b592d81aac9db9b726e953ae4a76c432f1871be4d88d82ddcd4beffea01c4fe674b6da8b53ae66c9ae43d342b499755'
+Crosswords::Application.config.secret_token = ENV['SECRET_TOKEN'] || '23971fce09f701c42949d73b3fb06bd90b592d81aac9db9b726e953ae4a76c432f1871be4d88d82ddcd4beffea01c4fe674b6da8b53ae66c9ae43d342b499755'
