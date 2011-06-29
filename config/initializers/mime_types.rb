eb94dd1c9d09e88ce298c26b41f929f056a181f1
Alex Crichton <alex@alexcrichton.com>
2011-06-29 00:31:36 -0700

    Typo, meant to_puz

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -8,6 +8,6 @@ Mime::Type.register_alias 'application/x-crossword', :puz
 
 ActionController::Renderers.add :puz do |obj, options|
   filename = options[:filename] || 'crossword.puz'
-  str = obj.respond_to?(:to_csv) ? obj.to_csv : obj.to_s
+  str = obj.respond_to?(:to_puz) ? obj.to_puz : obj.to_s
   send_data str, :type => Mime::PUZ
 end
