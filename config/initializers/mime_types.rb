201b72b2c67580ec5e0d1fdb47f3557eff15b002
Alex Crichton <alex@alexcrichton.com>
2011-06-29 00:34:21 -0700

    Also set the filename on the '.puz' files

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -9,5 +9,5 @@ Mime::Type.register_alias 'application/x-crossword', :puz
 ActionController::Renderers.add :puz do |obj, options|
   filename = options[:filename] || 'crossword.puz'
   str = obj.respond_to?(:to_puz) ? obj.to_puz : obj.to_s
-  send_data str, :type => Mime::PUZ
+  send_data str, :type => Mime::PUZ, :filename => filename
 end
