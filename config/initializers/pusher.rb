0d265cc96b1551e73cce8ac4adb431ff71e1c13d
Alex Crichton <alex@alexcrichton.com>
2011-09-03 21:39:30 -0400

    Pick up pusher config from PUSHER_URL

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,3 +1,5 @@
-Pusher.app_id = ENV['PUSHER_APP'] || '5975'
-Pusher.key    = ENV['PUSHER_KEY'] || '60ad04716e80aa8eae65'
-Pusher.secret = ENV['PUSHER_SECRET'] || '03216805993e774c89bc'
+if ENV['PUSHER_URL'].blank?
+  Pusher.app_id = '5975'
+  Pusher.key    = '60ad04716e80aa8eae65'
+  Pusher.secret = '03216805993e774c89bc'
+end
